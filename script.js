67beb3c79e0fa03e06cfc189672e1395337d858c
Anastasiia <105418918+Tarasova09@users.noreply.github.com>
2023-07-25 21:33:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+let rows = 3;
+let columns = 3;
+
+let currTile;
+let otherTile;
+
+let turns = 0;
+
+// let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9" ];
+let imgOrder = ["4", "1", "6", "8", "9", "7", "3", "5", "2" ];
+
+window.onload = function() {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      let tile = document.createElement("img");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.src = imgOrder.shift() + ".jpg";
+
+      tile.addEventListener("dragstart", dragStart);
+      tile.addEventListener("dragover", dragOver);
+      tile.addEventListener("dragenter", dragEnter);
+      tile.addEventListener("dragleave", dragLeave);
+      tile.addEventListener("drop", dragDrop);
+      tile.addEventListener("dragend", dragEnd);
+
+      document.getElementById("board").append(tile);
+    }
+  }
+}
+
+function dragStart() {
+  currTile = this;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+  otherTile = this;
+}
+
+function dragEnd() {
+
+  if (!otherTile.src.includes("3.jpg")) {
+    return;
+  }
+
+  let currCoords = currTile.id.split("-");
+  let r = parseInt(currCoords[0]);
+  let c = parseInt(currCoords[1]);
+
+  let otherCoords = otherTile.id.split("-");
+  let r2 = parseInt(otherCoords[0]);
+  let c2 = parseInt(otherCoords[1]);
+
+  let moveLeft = r == r2 && c2 == c - 1;
+  let moveRight = r == r2 && c2 == c + 1;
+
+  let moveUp = c == c2 && r2 == r - 1;
+  let moveDown = c == c2 && r2 == r + 1;
+
+  let isAdjascent = moveLeft || moveRight || moveUp || moveDown;
+
+  if (isAdjascent) {
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+  
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
+  }
+}
